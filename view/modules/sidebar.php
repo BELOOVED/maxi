99baa6509e56fe0837c04e9b172b8da77efc1390
BELOOVED <aycahanim@outlook.com>
2022-12-16 14:53:33 +0300

    modified:   index.php 	modified:   view/modules/header.php 	new file:   view/modules/sidebar.php

---
+++ view/modules/sidebar.php
@@ -0,0 +1,116 @@
+<slider-menu>
+  <section id="left-menu" class="slider-menu" style="top:0px;transition:all 0.3s ease 0s; transform: translateX(-100%);">
+    <div id="sidenav-overlay" class="slider-menu-background-overlay active"></div>
+    <div class="slider left-slider left-slider-open" test-id="left-slider-open" >
+      <div id="slider-wrapper" class="slider-wrapper" style="--scrollbar-width:0px;">
+        <game-search-normal class="menu-search">
+          <div name="search-form" class="game-search">
+            <div>
+              <div class="search-field">
+                <i class="fa icon-magnifier"></i>
+                <input test-id="game-search-normal-search-field-input" type="search" class="search-input ng-untouched ng-pristine ng-valid" placeholder="Oyunları Ara">
+              </div>
+              <div test-id="games-search-list" class="games-search-list">
+                <game-search-result test-id="search-game-result">
+                  <!---->
+                </game-search-result>
+                <!---->
+              </div>
+            </div>
+          </div>
+        </game-search-normal>
+        <div class="menu-content">
+          <a class="menu-item" id="login" href="login" test-id="nav-welcome-bonus"> Giriş Yap </a>
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+        </div>
+        <!---->
+        <!---->
+        <div class="menu-content">
+          <a class="menu-item" id="register" href="register" test-id="nav-welcome-bonus"> Kayıt Ol </a>
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+        </div>
+        <!---->
+        <!---->
+        <div class="menu-content">
+          <a class="menu-item" id="welcome-bonus" href="https://bonus.casinomaxi564.com/tr/1500-tl-150-freespin-hosgeldin-bonus-paketi/" test-id="nav-welcome-bonus"> Hoşgeldin Bonusu </a>
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+        </div>
+        <!---->
+        <!---->
+        <div class="menu-content">
+          <a class="menu-item" id="a-z" href="/tr/casino-oyunlari/" test-id="nav-a-z"> Tüm Oyunlar (A-Z) </a>
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+        </div>
+        <!---->
+        <!---->
+        <div class="menu-content">
+          <a class="menu-item" id="language" href="/tr/dil-secimi/" test-id="nav-language"> Dili değiştir </a>
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+        </div>
+        <!---->
+        <!---->
+        <div class="menu-content">
+          <a class="menu-item" id="casino-games" href="/tr/" test-id="nav-casino-games"> Anasayfam </a>
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+        </div>
+        <!---->
+        <!---->
+        <div class="menu-content">
+          <a class="menu-item" id="promotions" href="https://bonus.casinomaxi564.com/tr/" test-id="nav-promotions"> Kampanyalar </a>
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+        </div>
+        <!---->
+        <!---->
+        <div class="menu-content">
+          <a class="menu-item" id="support" href="https://hc.supportcasinomaxi.com/hc/tr" test-id="nav-support"> Yardım </a>
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+        </div>
+        <!---->
+        <!---->
+        <div class="menu-content">
+          <a class="menu-item" id="support" href="https://bonus.casinomaxi564.com/tr/en-kolay-para-yatirma-yontemleri/" test-id="nav-support"> Para Yatırma Yöntemleri </a>
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+          <!---->
+        </div>
+        <!---->
+        <!---->
+        <!---->
+      </div>
+    </div>
+  </section>
+</slider-menu>
