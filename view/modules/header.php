13bb41cb5ee04ef21cceac836d3cf793ca5257d1
BELOOVED <aycahanim@outlook.com>
2022-12-16 23:58:01 +0300

    modified:   view/modules/header.php

--- view/modules/header.php
+++ view/modules/header.php
@@ -1045,17 +1045,23 @@
     $(".right-slider").addClass("right-slider-closed").css("transition", ".4s");
     $("#slidercloseddoverlay").removeClass("active");
     });
-    $("#1accordion").click(function(){
-      $("#1submenu").css("height", "0px");
-    $("#1accordion").removeClass("open").css("transition", ".4s");
+    $("#closeacc1").click(function(){
+      $("#2submenu").css("height", "0px");
+    $("#2accordion").removeClass("open").css("transition", ".4s");
+    $("#3submenu").css("height", "0px");
+    $("#3accordion").removeClass("open").css("transition", ".4s");
     });
     $("#closeacc2").click(function(){
       $("#1submenu").css("height", "0px");
     $("#1accordion").removeClass("open").css("transition", ".4s");
+    $("#3submenu").css("height", "0px");
+    $("#3accordion").removeClass("open").css("transition", ".4s");
     });
     $("#closeacc3").click(function(){
-      $("#3submenu").css("height", "0px");
-    $("#3accordion").removeClass("open").css("transition", ".4s");
+      $("#1submenu").css("height", "0px");
+    $("#1accordion").removeClass("open").css("transition", ".4s");
+      $("#2submenu").css("height", "0px");
+    $("#2accordion").removeClass("open").css("transition", ".4s");
     });
 </script>
 <ui-view name="overlayBox">
